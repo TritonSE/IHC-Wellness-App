1e20ef06ed6442a18d38bb7bc202049866dfe7d1
rmtatum <rmtatum@ucsd.edu>
2019-10-28 14:40:11 -0700

    Init store and checkin changes

--- src/View/Screens/CheckInPage.tsx
+++ src/View/Screens/CheckInPage.tsx
@@ -1,14 +1,51 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Slider } from 'react-native';
 import AppHeader from '../../components/AppHeader';
 
 export default class CheckInPage extends React.Component<object, object> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mood: 5,
+      hoursOfSleep: 8
+    };
+  }
+
+  change(value) {
+   this.setState(() => {
+     return {
+       value: parseFloat(value),
+     };
+   });
+ }
+
   public render() {
+    const {value} = this.state;
     return (
       <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
         <AppHeader title="Check-in"/>
         <Text>Check In!</Text>
-        <Text>Be Happy!</Text>
+        <View style={{paddingTop: 30}}>
+          <Text>How are you feeling today?</Text>
+          <Slider
+            step={1}
+            minimumValue={1}
+            maximumValue={10}
+            thumbTintColor='rgb(252, 228, 149)'
+            onValueChange={val => this.setState({ mood: val })}
+            value={value}
+          />
+
+          <Text>How many hours of sleep did you get last night?</Text>
+          <Slider
+            step={1}
+            minimumValue={1}
+            maximumValue={10}
+            thumbTintColor='rgb(252, 228, 149)'
+            onValueChange={val => this.setState({ hoursOfSleep: val })}
+            value={value}
+          />
+        </View>
       </View>
     );
   }
--- src/View/Screens/StorePage.tsx
+++ src/View/Screens/StorePage.tsx
@@ -1,14 +1,45 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import AppHeader from '../../components/AppHeader';
 
 export default class StorePage extends React.Component<object, object> {
   public render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
         <AppHeader title="Store"/>
-        <Text>Store Page!</Text>
+        <Text style={styles.header}>Store Page!</Text>
+        <View styles={styles.shopRow}>
+          <TouchableOpacity sytle={styles.shopItem}>
+            <Text>Item 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity sytle={styles.shopItem}>
+            <Text>Item 2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity sytle={styles.shopItem}>
+            <Text>Item 3</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 30,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  shopRow: {
+    flexDirection: 'row'
+  },
+  container: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    backgroundColor: 'green'
+  },
+  shopItem: {
+    backgroundColor: 'blue',
+    padding: 5
+  }
+});
