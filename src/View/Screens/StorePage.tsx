745ae9e080e3adda6e4c343ab7af48b7d9657f7f
Rohith Kasar <rohith.kasar@gmail.com>
2020-04-20 20:45:29 -0700

    married owned and plantArray

--- src/View/Screens/StorePage.tsx
+++ src/View/Screens/StorePage.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Button, Modal } from 'react-native';
-import AppHeader from '../../components/AppHeader';
-import storeHelper from '../../Business/storeBackend'
+import { Button, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {flowers, pots, stems} from '../../Business/itemProperties.tsx';
 import plantHelper from '../../Business/plantBackend'
-import {pots, stems, flowers} from '../../Business/itemProperties.tsx';
-
+import storeHelper from '../../Business/storeBackend'
+import AppHeader from '../../components/AppHeader';
 
 export default class StorePage extends React.Component<object, object> {
   constructor() {
     super();
     this.state = {
       modalVisible: false,
-      item: {name: ""},
-      section: []
+      item: { name: ''},
+      section: [],
     };
   }
 
@@ -21,7 +20,7 @@ export default class StorePage extends React.Component<object, object> {
       <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
         <AppHeader title="Store"/>
         <Text style={styles.header}>Store Page!</Text>
-        <View styles={{flexDirection: 'row'}}>
+        <View styles={{ flexDirection: 'row' }}>
           <TouchableOpacity style={styles.shopItem}>
             <Text>Item 1</Text>
           </TouchableOpacity>
@@ -33,49 +32,47 @@ export default class StorePage extends React.Component<object, object> {
           </TouchableOpacity>
           <Button
           title="Buy Terracotta"
-          onPress={ ()=>
-            {
-            storeHelper.buyItem("terracotta","footers")
-
-          } }
+          onPress={ () => { storeHelper.buyItem('terracotta', 'footers'); } }
           />
           <Button
           title="Buy Long"
-          onPress={ ()=>{storeHelper.buyItem("long","bodies")} }
+          
+          onPress={ () => {storeHelper.buyItem('long', 'bodies'); } }
           />
           <Button
           title="Buy Short"
-          onPress={ ()=>{storeHelper.buyItem("short","bodies")} }
+          
+          onPress={ () => {storeHelper.buyItem('short', 'bodies'); } }
           />
 
           <Button
           title="Add Body"
-          onPress={ ()=>{plantHelper.addBody(0, "short")} }
+          onPress={ () => {plantHelper.addBody(0, 'short'); } }
           />
-          
+
           <Button
           title="Change Body"
-          onPress={ ()=>{plantHelper.changeBody(0, "short", 1, "long")} }
+          onPress={ () => {plantHelper.changeBody(0, 'short', 1, 'long'); } }
           />
 
         <Button
           title="createDefault"
-          onPress={ ()=>{
-            storeHelper.createOwned()
-            plantHelper.createDefaultPlantArray()
+          onPress={ () => {
+            storeHelper.createOwned();
+            plantHelper.createDefaultPlantArray();
           } }
           />
         <Button
           title="header"
-          onPress={ ()=>{plantHelper.getHeader(0)} }
+          onPress={ () => {plantHelper.getHeader(0); } }
         />
         <Button
           title="body"
-          onPress={ ()=>{plantHelper.getBody(0)} }
+          onPress={ () => {plantHelper.getBody(0); } }
         />
         <Button
           title="footer"
-          onPress={ ()=>{plantHelper.getFooter(0)} }
+          onPress={ () => {plantHelper.getFooter(0); } }
         />
 
         <Modal
@@ -88,9 +85,9 @@ export default class StorePage extends React.Component<object, object> {
                   <Button
                     title="Buy item"
                     onPress={async () => {
-                        this.setState({modalVisible: false });
-                        storeHelper.buyItem(this.state.item.name, this.state.section)
-                      }
+                      this.setState({ modalVisible: false });
+                      storeHelper.buyItem(this.state.item.name, this.state.section);
+                    }
                     }
                   />
                   <Button
@@ -111,15 +108,15 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 30,
     flexDirection: 'column',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
   },
   container: {
     paddingLeft: 30,
     paddingRight: 30,
-    backgroundColor: 'green'
+    backgroundColor: 'green',
   },
   shopItem: {
     padding: 5,
-    backgroundColor: 'blue'
-  }
+    backgroundColor: 'blue',
+  },
 });
