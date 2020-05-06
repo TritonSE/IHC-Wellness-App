032e373e60f1c57b051c3f3005e1ccc32df3a1e9
Glenn Raskovich <graskovich@gmail.com>
2020-05-05 17:59:04 -0700

    Update plantItems, plan to replace itemProperties.tsx with plantItems.ts

--- constants/plantItems.ts
+++ constants/plantItems.ts
@@ -1,3 +1,44 @@
-export const topItems = ['sunflower', 'leafTop']
-export const bodyItems = ['daisy', 'rose', 'tulip', 'buttercup']
-export const potItems = ['starter', 'terracotta']
+export interface IStoreItem {
+  img: string;
+  name: string;
+  price: number;
+}
+
+export const plantHeaders: IStoreItem[] = [
+  {
+    img: 'terracotta.png',
+    name: 'sunflower',
+    price: 1.25,
+  },
+  {
+    img: 'terracotta.png',
+    name: 'daisy',
+    price: 1.25,
+  },
+];
+
+export const plantBodyItems: IStoreItem[] = [
+  {
+    img: 'terracotta.png',
+    name: 'long',
+    price: 1.25,
+  },
+  {
+    img: 'terracotta.png',
+    name: 'short',
+    price: 1.25,
+  },
+];
+
+export const plantFooters: IStoreItem[] = [
+  {
+    img: 'terracotta.png',
+    name: 'terracotta',
+    price: 1.25,
+  },
+  {
+    img: 'terracotta.png',
+    name: 'clay',
+    price: 1.25,
+  },
+];
